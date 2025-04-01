e86f09cec0f056076b3d02973bd7950a45b2d0f1
vishu <vanshsinghsre@gmail.com>
2025-04-02 00:12:11 +0530

    final

--- MYoffice/code/Project1/modules/vpc/mains.tf
+++ MYoffice/code/Project1/modules/vpc/mains.tf
@@ -14,23 +14,24 @@ tags = {
 }
 
 resource "aws_subnet" "pubsub" {
-    count = length(pubsub_cidr)
+    count = length(var.pubsub_cidr)
   vpc_id = aws_vpc.vpc1.id
   cidr_block = var.pubsub_cidr[count.index]
   availability_zone = var.az[count.index]
   tags = {
-    Name = "${var.env}-pub_sub${count}"
+    Name = "${var.env}-pub_sub${count.index}"
   }
 
 }
 
 resource "aws_subnet" "privsub" {
-    count = length(privsub_cidr)
+    count = length(var.privsub_cidr)
   vpc_id = aws_vpc.vpc1.id
   cidr_block = var.privsub_cidr[count.index]
   availability_zone = var.az[count.index]
-  tags = "${var.env}-priv_sub${count}"
-
+  tags = {
+    Name = "${var.env}-priv_sub${count.index}"
+  }
 }
 
 resource "aws_internet_gateway" "ig1" {
@@ -45,7 +46,8 @@ resource "aws_internet_gateway_attachment" "ig1-att" {
 
 resource "aws_route_table" "rt1" {
   vpc_id = aws_vpc.vpc1.id
-  tags = "${var.env}-pubrt"
+  tags = {
+    Name = "${var.env}-pubrt" }
 }
 
 resource "aws_route" "routes1" {
@@ -55,14 +57,17 @@ resource "aws_route" "routes1" {
 }
 
 resource "aws_route_table_association" "rt1-att" {
-    count = length(pubsub_cidr)
+    count = length(var.pubsub_cidr)
   route_table_id = aws_route_table.rt1.id
   subnet_id = aws_subnet.pubsub[count.index].id
 }
 
 resource "aws_security_group" "sg1" {
   vpc_id = aws_vpc.vpc1.id
-tags = "${var.env}-pub_sg"
+tags = {
+    Name = "${var.env}-pub_sg"
+
+}
 
 }
 
