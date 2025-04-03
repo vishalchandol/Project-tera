782d6c1827dcfdf26b1d610e72dfedfd268ac7d5
vishu <vanshsinghsre@gmail.com>
2025-04-03 10:05:23 +0530

    ec2 removed

--- MYoffice/code/Project1/modules/ec2/mains.tf
+++ MYoffice/code/Project1/modules/ec2/mains.tf
@@ -1,21 +1,4 @@
 
-resource "aws_instance" "inst1" {
-  instance_type = "t2.micro"
-  #ami = "ami-00224e59617d0d55e"
-  #ami = "ami-015612a97cb1d6952"
-  ami = "ami-071226ecf16aa7d96"
- 
-  subnet_id = var.sub_id[0]
-  associate_public_ip_address = true
-  tags = {
-    name = "${var.env}inst"
-  }
-
-  vpc_security_group_ids = var.sg_inst
-  source_dest_check = false
- # key_name = aws_key_pair.kp1.key_name
-
-}
 
 resource "aws_instance" "inst2" {
   instance_type = "t2.micro"
